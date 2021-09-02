898e8298b9d5dba57b78cd9df7bf3a17773c4f5f
Alexander Biezbardis <mr.biezbardis@gmail.com>
2021-09-02 23:54:34 +0300

    Feature commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,21 +1,34 @@
 package org.example;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.*;
 
 public class App {
     private static final ExecutorService THREAD_POOL = Executors.newFixedThreadPool(4);
-    
-    public static void main(String[] args) throws InterruptedException {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         System.out.println("Lets get the multithreading started!");
 
-        THREAD_POOL.submit(new LocalThread("ONE"));
-        THREAD_POOL.submit(new LocalThread("TWO"));
-        THREAD_POOL.submit(new LocalThread("THREE"));
-        THREAD_POOL.submit(new LocalThread("FOUR"));
+        Set<Callable<String>> callableSet = new HashSet<>();
+        callableSet.add(new LocalThread("ONE"));
+        callableSet.add(new LocalThread("TWO"));
+        callableSet.add(new LocalThread("THREE"));
+        callableSet.add(new LocalThread("FOUR"));
+
+        List<Future<String>> result_1 = THREAD_POOL.invokeAll(callableSet);
+        String result_2 = THREAD_POOL.invokeAny(callableSet);
 
         Thread.sleep(15_000);
         THREAD_POOL.shutdownNow();
+
+        for (Future<String> task : result_1) {
+            System.out.println(task.get());
+        }
+
+        System.out.println(result_2);
+
         System.out.println("Task has been finished. Have a nice day!");
     }
 }
--- src/main/java/org/example/LocalThread.java
+++ src/main/java/org/example/LocalThread.java
@@ -1,24 +1,32 @@
 package org.example;
 
-class LocalThread extends Thread {
+import java.util.concurrent.Callable;
+
+class LocalThread implements Callable<String> {
     private final int PAUSE = 2500;
     private final String NAME;
+    private int counter = 0;
 
     LocalThread(String name) {
         NAME = "Thread " + name;
     }
 
     @Override
-    public void run() {
+    public String call() {
+
         try {
-            while (!isInterrupted()) {
+            while (!Thread.interrupted()) {
                 Thread.sleep((int) (PAUSE * Math.random()));
-                System.out.printf("%s is greeting you!\n", getName());
+                System.out.printf("%s is greeting you!\n", this.NAME);
+                this.counter++;
             }
         } catch (InterruptedException err) {
-            System.out.printf("%s has been interrupted.\n", getName());
+            System.out.printf("%s has been interrupted.\n", this.NAME);
         } finally {
-            System.out.printf("%s has been stopped!\n", getName());
+            System.out.printf("%s has been stopped!\n", this.NAME);
         }
+
+        return "Result: task of thread " + this.NAME + " has been done "
+                + this.counter + "times.";
     }
 }
